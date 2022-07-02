a3e3eee2cdc16ca93b1c6a7651f7785d03ad3384
EnesSERENLI <enes.serenli@hotmail.com>
2022-07-02 19:11:13 +0300

    AutoMapper Added

---
+++ HC.Application/AutoMapper/Mapping.cs
@@ -0,0 +1,25 @@
+﻿using AutoMapper;
+using HC.Application.Models.DTO;
+using HC.Domain.Entities.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HC.Application.AutoMapper
+{
+    public class Mapping : Profile
+    {
+        public Mapping()
+        {
+            CreateMap<Product, CreateProductDTO>().ReverseMap();
+            CreateMap<Department, CreateDepartmentDTO>().ReverseMap();
+            CreateMap<Category, CreateCategoryDTO>().ReverseMap();
+            CreateMap<SubCategory, CreateSubCategoryDTO>().ReverseMap();
+            CreateMap<Employee, CreateEmployeeDTO>().ReverseMap();
+            CreateMap<AppUser, LoginDTO>().ReverseMap();
+            CreateMap<AppUser, RegisterDTO>().ReverseMap();
+        }
+    }
+}

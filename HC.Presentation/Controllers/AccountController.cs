a6af872dc68b80349769d39c38a037ba33b282c7
EnesSERENLI <enes.serenli@hotmail.com>
2022-07-07 20:44:25 +0300

    Account controller created. Login will be done.

---
+++ HC.Presentation/Controllers/AccountController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace HC.Presentation.Controllers
+{
+    public class AccountController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}

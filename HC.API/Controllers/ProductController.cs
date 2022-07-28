0e4cfc6b3f626207b230fec475a5ec9d11dfd829
EnesSERENLI <enes.serenli@hotmail.com>
2022-07-28 16:19:27 +0300

    API project created. Endpoints will be written!

---
+++ HC.API/Controllers/ProductController.cs
@@ -0,0 +1,24 @@
+﻿using HC.Application.Models.VM;
+using HC.Application.Service.Interface;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace HC.API.Controllers
+{
+    [Route("api/[controller]s")] //Using s in API controllers can be nice for clarity.
+    [ApiController]
+    public class ProductController : ControllerBase
+    {
+        private readonly IProductService _productService;
+
+        public ProductController(IProductService productService)
+        {
+            _productService = productService;
+        }
+        [HttpGet]
+        public async Task<IActionResult> GetProducts()
+        {
+            return Ok(await _productService.GetDefaultProducts());
+        }
+    }
+}
---
+++ HC.API/Program.cs
@@ -0,0 +1,91 @@
+using AutoMapper;
+using HC.Application.AutoMapper;
+using HC.Application.Service.Concrete;
+using HC.Application.Service.Interface;
+using HC.Domain.UnitOfWork;
+using HC.Infrastructure.Context;
+using HC.Infrastructure.UnitOfWork;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Identity.Web;
+using Microsoft.OpenApi.Models;
+using System.Reflection;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+    .AddMicrosoftIdentityWebApi(builder.Configuration.GetSection("AzureAd"));
+
+builder.Services.AddControllers();
+
+var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
+builder.Services.AddDbContext<HotCatDbContext>(options =>
+{
+    options.UseSqlServer(connectionString);
+});
+
+builder.Services.AddTransient<IUnitOfWork, UnitOfWork>();
+builder.Services.AddTransient<IProductService, ProductService>();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+
+//AutoMapper
+builder.Services.AddAutoMapper(Assembly.GetExecutingAssembly());
+
+//Mapperconfig
+var mapperConfig = new MapperConfiguration(cfg =>
+{
+    cfg.AddProfile(new Mapping());
+});
+builder.Services.AddSingleton(mapperConfig.CreateMapper());
+
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen(options =>
+{
+    options.SwaggerDoc("HCAPI", new OpenApiInfo()
+    {
+        Title = "RESTful API",
+        Version = "v1",
+        Description = "Restful Api Trials",
+        Contact = new OpenApiContact()
+        {
+            Email = "enes.serenli@hotmail.com",
+            Name = "Enes Serenli",
+            Url = new Uri("https://github.com/EnesSERENLI")
+        },
+        License = new OpenApiLicense()
+        {
+            Name = "MIT Licence",
+            Url = new Uri("https://opensource.org/licenses/MIT")
+        }
+    });
+});
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseDeveloperExceptionPage();    
+}
+
+app.UseSwagger();
+app.UseSwaggerUI(options =>
+{
+    options.SwaggerEndpoint("/swagger/HCAPI/swagger.json", "HCAPI");
+});
+
+app.UseRouting();
+
+app.UseHttpsRedirection();
+
+app.UseAuthentication();
+app.UseAuthorization();
+
+app.UseEndpoints(endPoints =>
+{
+    endPoints.MapControllers();
+});
+
+app.Run();
